1ce5db32cf5a079325723703d475358be956c846
Piotr Yordanov <piotr@piotryordanov.com>
2026-08-12 19:24:26 +0700

    feat: upgrades to tsdown

--- apps/tui/tsdown.config.ts
+++ apps/tui/tsdown.config.ts
@@ -27,4 +27,5 @@ export default defineConfig({
       'react',
     ],
   },
+  exe: true
 })
--- packages/pi-platform/tsdown.config.ts
+++ packages/pi-platform/tsdown.config.ts
@@ -6,4 +6,7 @@ export default defineConfig({
   dts: true,
   clean: true,
   target: 'node20',
+  deps: {
+    neverBundle: true,
+  },
 })
