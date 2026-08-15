1aaaf22902c5fce99df9a494795df612784b2dbc
Piotr Yordanov <piotr@piotryordanov.com>
2026-08-15 23:51:54 +0700

    chore: fix warnings

--- packages/feature-flags/src/featureManagement/registerFeatureManagementExtensionWithConfig.ts
+++ packages/feature-flags/src/featureManagement/registerFeatureManagementExtensionWithConfig.ts
@@ -1,6 +1,6 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import { withSlashMenuGroup } from "@extensions/shared";
-import { showFeaturesModal } from "../command/showFeaturesModal";
+import { showFeaturesModal } from "./command/showFeaturesModal";
 
 /**
  * Registers the feature management command.
